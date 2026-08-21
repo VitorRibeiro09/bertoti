daad1ad95c381eb73be87975b1b12f8439ab2a16
VitorRibeiro09 <vitorsamulmi09@gmail.com>
2026-08-21 16:46:32 -0300

    Adicionando projeto sobre agente de IA financeiro, para o trabalho da aula do professor Giuliano Bertoti

---
+++ Padrões de Projetos/FinMindsAI/FinMindsAI/src/main/java/br/com/fatec/FinMindsAI/FinMindsAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.FinMindsAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinMindsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinMindsAiApplication.class, args);
+	}
+
+}
---
+++ Padrões de Projetos/FinMindsAI/FinMindsAI/src/test/java/br/com/fatec/FinMindsAI/FinMindsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.FinMindsAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinMindsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
